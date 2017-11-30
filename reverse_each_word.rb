8cc4b8feff6eb6fe139a139e6c445628d726c4b4
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-30 18:46:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 
-def reverse_each_word(sentence1)
-  array = sentence1.split{","}
-  array2 = array.collect do |word|
-    word.reverse
-  end
-  array2.join(" ")
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
